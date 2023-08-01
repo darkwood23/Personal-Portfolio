4fd5203a6414dea64882a3d992ceb59b3eb48f04
darkwood23 <anonymoushackerintheweb@gmail.com>
2023-08-01 22:02:56 +0545

    All done

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
 const github = document.getElementById("github-icon")
 
+const bomb = document.getElementById("bomb")
+const bombGit = document.getElementById("bomb-github")
+
+const tic = document.getElementById("tic")
+const ticGit = document.getElementById("tic-github")
+
+const admin = document.getElementById("admin")
+const adminGit = document.getElementById("admin-github")
+
+const restaurant = document.getElementById("restaurant")
+const restaurantGit = document.getElementById("restaurant-github")
+
+const login = document.getElementById("login")
+const loginGit = document.getElementById("login-github")
+
+const todo = document.getElementById("todo")
+const todoGit = document.getElementById("todo-github")
+
 github.addEventListener("click", function() {
     window.open("https://github.com/darkwood23")
+})
+
+bomb.addEventListener('click', function() {
+    window.open("https://darkwood23.github.io/Hydrogen-bomb")
+})
+
+bombGit.addEventListener('click', function() {
+    window.open("https://github.com/darkwood23/Hydrogen-bomb")
+})
+
+tic.addEventListener("click", function() {
+    window.open("https://darkwood23.github.io/Tic-Tac-Toe")
+})
+
+ticGit.addEventListener("click", function() {
+    window.open("https://github.com/darkwood23/Tic-Tac-Toe")
+})
+
+admin.addEventListener("click", function() {
+    window.open("https://darkwood23.github.io/Admin-Dashboard")
+})
+
+adminGit.addEventListener("click", function() {
+    window.open("https://github.com/darkwood23/Admin-Dashboard")
+})
+
+restaurant.addEventListener("click", function() {
+    window.open("https://darkwood23.github.io/Restaurant-Page")
+})
+
+restaurantGit.addEventListener("click", function() {
+    window.open("https://github.com/darkwood23/Restaurant-Page")
+})
+
+login.addEventListener("click", function() {
+    window.open("https://darkwood23.github.io/Login-Page")
+})
+
+loginGit.addEventListener("click", function() {
+    window.open("https://github.com/darkwood23/Login-Page")
+})
+
+todo.addEventListener('click', function() {
+    window.open("https://darkwood23.github.io/To-Do-List")
+})
+
+todoGit.addEventListener("click", function() {
+    window.open("https://github.com/darkwood23/To-Do-List")
 })
